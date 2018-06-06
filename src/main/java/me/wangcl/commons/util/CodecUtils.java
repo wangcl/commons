d496e2cc7da1290300a7ad8fb0597da9b1c2b449
wangcl <wangcl@live.com>
2018-06-06 22:08:02 +0800

    增加byte[]数据的base64编码

--- src/main/java/me/wangcl/commons/util/CodecUtils.java
+++ src/main/java/me/wangcl/commons/util/CodecUtils.java
@@ -12,17 +12,6 @@ import java.io.UnsupportedEncodingException;
 public class CodecUtils {
 
 	/**
-	 * 将字符串进行base64编码
-	 *
-	 * @param data 待加密字符串
-	 * @param charset 字符编码
-	 * @return
-	 */
-	public static String base64Encode(String data, String charset) {
-		return Base64.encodeBase64String(getBytes(data, charset));
-	}
-
-	/**
 	 * 将字符串按指定编码转换成字节数组。
 	 *
 	 * @param s 字符串
@@ -42,6 +31,37 @@ public class CodecUtils {
 		} catch (UnsupportedEncodingException e) {
 			return null;
 		}
+	}
+
+	/**
+	 * 将字符串进行base64编码
+	 *
+	 * @param data 待加密字符串
+	 * @param charset 字符编码
+	 * @return
+	 */
+	public static String encodeBase64(String data, String charset) {
+		return Base64.encodeBase64String(getBytes(data, charset));
+	}
+
+	/**
+	 * 将字节数据进行base64编码
+	 *
+	 * @param binaryData
+	 * @return
+	 */
+	public static byte[] encodeBase64(byte[] binaryData) {
+		return Base64.encodeBase64(binaryData);
+	}
+
+	/**
+	 * 将字节数据进行base64编码
+	 *
+	 * @param binaryData
+	 * @return
+	 */
+	public static String encodeBase64String(byte[] binaryData) {
+		return Base64.encodeBase64String(binaryData);
 	}
 
 }
