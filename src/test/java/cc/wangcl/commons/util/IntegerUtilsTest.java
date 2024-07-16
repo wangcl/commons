533883339c69efbd3fcb91c302df02534295d0c8
wangcl <wangcl@live.com>
2024-07-16 21:09:41 +0800

    add IntegerUtilsTest

---
+++ src/test/java/cc/wangcl/commons/util/IntegerUtilsTest.java
@@ -0,0 +1,32 @@
+package cc.wangcl.commons.util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IntegerUtilsTest {
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}
