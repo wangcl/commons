1eb4cd7c0c58157cda2e38fd05f65326411ef88c
wangcl <wangcl@live.com>
2024-07-17 20:14:35 +0800

    move methods from IntegerUtils to StringUtils

--- src/main/java/cc/wangcl/commons/util/StringUtils.java
+++ src/main/java/cc/wangcl/commons/util/StringUtils.java
@@ -155,4 +155,33 @@ public class StringUtils {
 		return sb.toString();
 	}
 
+	/**
+	 * Convert an integer to 32-bits binary string with leading zeros.
+	 *
+	 * @param i integer
+	 * @return binary string
+	 */
+	public static String toBinaryString(int i) {
+		return toBinaryString(i, 4);
+	}
+
+	/**
+	 * Convert an integer to binary string with leading zeros.
+	 *
+	 * @param i        integer
+	 * @param byteSize size of bytes
+	 * @return binary string
+	 */
+	public static String toBinaryString(int i, int byteSize) {
+		if (byteSize > 4 || byteSize <= 0) byteSize = 4;
+		String s = Integer.toBinaryString(i);
+		int diffSize = 8 * byteSize - s.length();
+		StringBuilder sb = new StringBuilder();
+		for (int j = 0; j < diffSize; j++) {
+			sb.append("0");
+		}
+		sb.append(s);
+		return sb.toString().toUpperCase();
+	}
+
 }
